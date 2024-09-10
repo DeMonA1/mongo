f15586b277a256f65c5bc2b86dd5ade7d465f954
DeMonA1 <demonagrail1@gmail.com>
2024-09-10 12:10:28 +0300

    ''''

--- create_cluster.sh
+++ create_cluster.sh
@@ -1,4 +1,4 @@
- mkdir -p ./mongoconfig
+mkdir -p ./mongoconfig
 mongod --configsvr --replSet configReplSet --dbpath ./mongoconfig --port 27016
 mongosh localhost:27016
 rs.initiate({
@@ -8,21 +8,22 @@ rs.initiate({
        ]
    });
    
-     mkdir -p ./mongo1 ./mongo2
- mongod --shardsvr --replSet shardReplSet --dbpath ./mongo1 --port 27014
-  mongod --shardsvr --replSet shardReplSet --dbpath ./mongo2 --port 27015
-  mongosh localhost:27014
+mkdir -p ./mongo1 ./mongo2
+mongod --shardsvr --replSet shardReplSet --dbpath ./mongo1 --port 27014
+mongod --shardsvr --replSet shardReplSet --dbpath ./mongo2 --port 27015
+mongosh localhost:27014
   rs.initiate({
-       _id: "shardReplSet",
-       members: [
+        _id: "shardReplSet",
+        members: [
            { _id: 0, host: "localhost:27014" },
            { _id: 1, host: "localhost:27015" }  // Add this member only when configuring the second shard
        ]
    });
 mongos --configdb configReplSet/localhost:27016 --port 27020
-mongosh localhost:27017
+mongosh localhost:27020
 use admin;  // Switch to the admin database
 
-   db.runCommand({ addShard: "shardReplSet/localhost:27014" });
-   db.runCommand({ addShard: "shardReplSet/localhost:27015" });
-   
+db.runCommand({ addShard: "shardReplSet/localhost:27014" });
+db.runCommand({ addShard: "shardReplSet/localhost:27015" });
+db.runCommand({ enablesharding : 'test' })
+db.runCommand({shardcollection: 'test.cities', key: {name : 1}})
